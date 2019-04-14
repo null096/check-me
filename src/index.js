bbcaf2230a1f29a6346e7cc7beee1367692e176e
null096 <33376471+null096@users.noreply.github.com>
2019-04-14 11:49:30 +0300

    checkMe works as a class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-const checkMe = (text) => {
-  if (typeof text !== 'string') {
-    throw new Error('Text must be a string');
+class checkMe {
+  constructor(text) {
+    this.setTextData(text);
   }
 
-  return {
-    text
-  };
+  setTextData(text) {
+    this.validateText(text);
+
+    this.text = text.trim();
+    this.textParts = this.text.split(/\s+/);
+    this.textSeparators = this.text.match(/\s+/g);
+    this.currentWordIndex = 0;
+    this.isError = false;
+  }
+
+  validateText(text) {
+    if (typeof text !== 'string') {
+      throw new Error('Text must be a string');
+    }
+    text = text.trim();
+    if (!text) {
+      throw new Error('Text must contain at least one word');
+    }
+
+    return true;
+  }
 }
 
 export default checkMe;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,30 @@
 import checkMe from '../src';
 
 describe('CheckMe takes only a string', () => {
+  const err = 'Text must be a string';
+
   it('With a string', () => {
     const text = 'my text';
-    expect(checkMe(text)).toEqual({ text });
+    expect(() => new checkMe(text)).not.toThrowError(err);
   });
   it('Not with a string', () => {
-    const err = 'Text must be a string';
-    expect(() => checkMe()).toThrowError(err);
-    expect(() => checkMe(3)).toThrowError(err);
-    expect(() => checkMe(false)).toThrowError(err);
-    expect(() => checkMe({})).toThrowError(err);
-    expect(() => checkMe(() => { })).toThrowError(err);
+    expect(() => new checkMe()).toThrowError(err);
+    expect(() => new checkMe(3)).toThrowError(err);
+    expect(() => new checkMe(false)).toThrowError(err);
+    expect(() => new checkMe({})).toThrowError(err);
+    expect(() => new checkMe(() => { })).toThrowError(err);
   });
+});
+
+describe('Throw error with empty strings', () => {
+  const err = 'Text must contain at least one word';
+  it('With empty string', () => {
+    expect(() => new checkMe('')).toThrowError(err);
+    expect(() => new checkMe('\r\t  \n  \t')).toThrowError(err);
+  });
+  it('With normal string', () => {
+    expect(() => new checkMe('normal one')).not.toThrowError(err);
+    expect(() => new checkMe('qwerty  \t')).not.toThrowError(err);
+    expect(() => new checkMe(' \ralso fine \t')).not.toThrowError(err);
+  })
 });
